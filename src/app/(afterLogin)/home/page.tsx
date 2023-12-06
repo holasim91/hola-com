67737f9bbd6fbbe0126ff40232fb018870a0a665
Hyunin Sim <holasim91@gmail.com>
2023-12-06 15:03:46 +0900

    day 2 1/2 complete

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,10 +1,22 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import style from "@/app/(afterLogin)/home/home.module.css"
+import Tab from "./_component/Tab"
+import PostForm from "./_component/PostForm"
+import TabProvider from "./_component/TabProvider"
+import Post from "../_component/Post"
 
 export default function Home() {
   return (
-    <main>
-      홈페이지
+    <main className={style.main}>
+      <TabProvider>
+      <Tab />
+      <PostForm />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      </TabProvider>
     </main>
      )
 }
